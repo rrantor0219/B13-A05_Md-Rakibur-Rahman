93eb14ac4c500612d2575f7f037041819fe4894f
Md Rakibur Rahman <rrantor0219@gmail.com>
2026-03-09 06:40:25 +0800

    implement issue details modal using single issue api

--- script.js
+++ script.js
@@ -78,10 +78,35 @@ card.innerHTML = `
 <p>${issue.createdAt}</p>
 `;
 
+card.onclick = () => openModal(issue.id);
 container.appendChild(card);
 
 });
 
+}
+// open modal
+
+async function openModal(id){
+
+const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+const data = await res.json();
+
+const issue = data.data;
+
+document.getElementById("modalTitle").innerText = issue.title;
+document.getElementById("modalDescription").innerText = issue.description;
+document.getElementById("modalStatus").innerText = issue.status;
+document.getElementById("modalAuthor").innerText = issue.author;
+document.getElementById("modalPriority").innerText = issue.priority;
+document.getElementById("modalLabel").innerText = issue.label;
+document.getElementById("modalDate").innerText = issue.createdAt;
+
+document.getElementById("issueModal").style.display="flex";
+
+}
+
+function closeModal(){
+document.getElementById("issueModal").style.display="none";
 }
 
 // load automatically
