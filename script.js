a4fea08be059c613b614024b845657f22187a402
Md Rakibur Rahman <rrantor0219@gmail.com>
2026-03-09 07:27:27 +0800

    fix small UI issues and finalize project

--- script.js
+++ script.js
@@ -2,17 +2,20 @@
 
 function loginUser(){
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
 
-    if(username === "admin" && password === "admin123"){
-        localStorage.setItem("loggedIn","true");
-        window.location.href = "index.html";
-    }else{
-        alert("Wrong username or password");
-    }
+if(username === "admin" && password === "admin123"){
+localStorage.setItem("loggedIn","true");
+window.location.href = "index.html";
+}else{
+alert("Wrong username or password");
+}
 
 }
+
+// API url
+const apiURL = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
 // load issues
 
 async function loadIssues(type="all"){
@@ -131,8 +134,7 @@ loading.style.display = "none";
 }
 async function searchIssues(){
 
-const text = document.getElementById("searchInput").value;
-
+const text = document.getElementById("searchInput").value.trim();
 if(!text) return;
 
 showLoading(true);
